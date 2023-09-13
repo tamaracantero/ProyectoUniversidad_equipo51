13dc2ce6afd8b85d6e4e7c51b8d3f9afd45b5e23
JuanLaspiur <laspiurperezjuan@gmail.com>
2023-09-13 11:39:50 -0300

    BRIAN PEREIRA agregó ActualizarMateria(Materia m)

--- Universidad/src/controladores/ControladorMateria.java
+++ Universidad/src/controladores/ControladorMateria.java
@@ -44,4 +44,29 @@ public class ControladorMateria {
                System.out.println("Error en Obtener idMateria,por favor intente de nuevo");
         }
     }
+    
+     /**HECHO POR BRIAN PEREIRA*/
+    public static void ActualizarMateria(Materia m) {
+        String sql = "UPDATE materia SET Nombre=?,Anio=?,Estado=? WHERE idMateria=?";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, m.getNombre());
+            ps.setInt(2, m.getAnio());
+            ps.setInt(3, m.getEstado());
+            ps.setInt(4, m.getIdMateria());
+            int f = ps.executeUpdate();
+            if (f > 0) {
+                JOptionPane.showMessageDialog(null, "Materia Actualizada Correctamente");
+                System.out.println("Filas Afectadas: " + f);
+            } else {
+                JOptionPane.showMessageDialog(null, "La Materia no se ha Actualizado Correctamente");
+                System.out.println("Filas Afectadas: " + f);
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al Agregar una materia a la Base de Datos " + ex.getMessage());
+            System.out.println("Error en en el Metodo crearMateria, Clase ControladorMateria.");
+        }
+    }
+    
+    
 }
