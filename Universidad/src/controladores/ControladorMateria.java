4594d51cce731a19339099b9c42dfaa99f605bef
tamaracantero <141643686+tamaracantero@users.noreply.github.com>
2023-09-13 11:50:42 -0300

    2° intento subir metodo subirMateria

--- Universidad/src/controladores/ControladorMateria.java
+++ Universidad/src/controladores/ControladorMateria.java
@@ -20,7 +20,30 @@ public class ControladorMateria {
     static Connection con= Conexion.conectarse();
     static PreparedStatement ps = null;
     static ResultSet rs = null;
+    /**HECHO POR TAMARA CANTERO*/
+    public static void subirMateria(Materia materia){
+        String sql = "INSERT INTO materia (nombre,anio,estado) VALUES (?,?,?) ";
+        try {
+            
+            ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1,materia.getNombre());
+            ps.setInt(2, materia.getAnio());
+            ps.setInt(3, materia.getEstado());
+            
+            ps.executeUpdate();
+            
+            rs = ps.getGeneratedKeys();
+            if (rs.next()){
+            materia.setIdMateria(rs.getInt(1));
+             JOptionPane.showMessageDialog(null, "Materia agregada con exito.");
+            }
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al Agregar una materia a la Base de Datos "+ex.getMessage());
+            System.out.println("Error en en el Metodo subirMateria, Clase ControladorMateria.");
+        }
     
+    }
     /**HECHO POR ARIEL LAZARTE*/
     
     public static void obtenerMateriaPorId(int idMateria) {
